be397e247a71455cd2afbc927b60a16f64d9222a
sultanashraf14 <sultanashraf.jobs@gmail.com>
2024-01-16 12:35:14 +0530

    episode-01

---
+++ App.js
@@ -0,0 +1,19 @@
+/**
+ * lkjkjl
+ * <div id="parent">
+        <div id="child">
+            <h1></h1>
+        </div>
+        <div id="child2">
+            <h1></h1>
+        </div>
+    </div>
+ * 
+ */
+
+const parent = React.createElement("div", { id: "parent" }, [React.createElement("div", { id: "child" }, [React.createElement("h1", {}, "I am h1 tag"), React.createElement("h2", {}, "I am h2 tag")]), React.createElement("div", { id: "child2" }, React.createElement("h3", {}, "I am t3 tag"))]);
+
+const heading = React.createElement("h1", { id: "heading" }, "Hello World from React!");
+console.log(parent);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent);
